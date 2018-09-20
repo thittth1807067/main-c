3c4241d696392c1081374026510bccb0d403e8fd
Thi TT <thittth1807067@fpt.edu.vn>
2018-09-20 15:31:36 +0700

    Khoi tao project

---
+++ cmake-build-debug/CMakeFiles/myc_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myc_demo.dir/main.c.o"
+  "myc_demo.pdb"
+  "myc_demo.exe"
+  "libmyc_demo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myc_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
